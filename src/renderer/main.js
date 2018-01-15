29f7a19f75860e1fb1f54397bb6d89715e11d277
codertx <codert.sn@gmail.com>
2018-01-15 21:42:39 +0800

    [contrib] Reconstruct

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
-import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import HotKey from '@/plugins/hot-key'
+import Element from '@/plugins/element-ui'
 import App from './App'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
@@ -12,6 +11,9 @@ Vue.use(Element)
 
 /* use hot key plguins */
 Vue.use(HotKey)
+
+/* use element-ui */
+Vue.use(Element)
 
 /* eslint-disable no-new */
 new Vue({
