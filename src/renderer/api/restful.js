01c935f02c870a40387ffcf51c33601bc2af1987
codertx <codert.sn@gmail.com>
2018-02-07 23:13:38 +0800

    [contrib] Add callback support

--- src/renderer/api/restful.js
+++ src/renderer/api/restful.js
@@ -15,23 +15,31 @@
 import HttpClient from './http-client'
 
 export default class RESTful {
-  constructor (url, options) {
+  constructor (url, options, resolve, reject) {
     this.baseURL = url
     this._httpClient = new HttpClient(options)
     this._entities = {}
+    this._resolve = resolve
+    this._reject = reject
   }
 
   registerEntity (url, alias, block = []) {
-    this._entities[alias] = new Entity(this._httpClient, url, alias, block)
+    this._entities[alias] = new Entity(this._httpClient, url, alias, block, this._resolve, this._reject)
+  }
+
+  getEntity (alias) {
+    return this._entities[alias]
   }
 }
 
 class Entity {
-  constructor (httpclient, url, alias, block) {
+  constructor (httpclient, url, alias, block, resolve, reject) {
     this._httpclient = httpclient
     this._url = url[url.length - 1] === '/' ? url.slice(0, -1) : url
     this._block = block
     this._alias = alias
+    this._resolve = resolve
+    this._reject = reject
   }
 
   update (id, data, params = null, headers = []) {
@@ -39,7 +47,10 @@ class Entity {
       return Promise.reject(new Error(`Update action on ${this._alias} has been blocked.`))
     }
 
-    return this._httpclient.put(this._url + `/${id}`, data, params, headers)
+    return this._httpclient
+      .put(this._url + `/${id}`, data, params, headers)
+      .then(this.resolve)
+      .catch(this.reject)
   }
 
   get (id, params = null, headers = []) {
@@ -47,7 +58,10 @@ class Entity {
       return Promise.reject(new Error(`Get action on ${this._alias} has been blocked.`))
     }
 
-    return this._httpclient.get(this._url + `/${id}`, params, headers)
+    return this._httpclient
+      .get(this._url + `/${id}`, params, headers)
+      .then(this.resolve)
+      .catch(this.reject)
   }
 
   list (params = null, headers = []) {
@@ -55,7 +69,10 @@ class Entity {
       return Promise.reject(new Error(`List action on ${this._alias} has been blocked.`))
     }
 
-    return this._httpclient.get(this._url, params, headers)
+    return this._httpclient
+      .get(this._url, params, headers)
+      .then(this.resolve)
+      .catch(this.reject)
   }
 
   create (data, params = null, headers = []) {
@@ -63,7 +80,10 @@ class Entity {
       return Promise.reject(new Error(`Create action on ${this._alias} has been blocked.`))
     }
 
-    return this._httpclient.post(this._url, data, params, headers)
+    return this._httpclient
+      .post(this._url, data, params, headers)
+      .then(this.resolve)
+      .catch(this.reject)
   }
 
   delete (id, data, params = null, headers = []) {
@@ -71,6 +91,9 @@ class Entity {
       return Promise.reject(new Error(`Delete action on ${this._alias} has been blocked.`))
     }
 
-    return this._httpclient.delete(this._url + `/${id}`, data, params, headers)
+    return this._httpclient
+      .delete(this._url + `/${id}`, data, params, headers)
+      .then(this.resolve)
+      .catch(this.reject)
   }
 }
